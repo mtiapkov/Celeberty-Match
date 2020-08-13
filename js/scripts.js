222c2916536a56989e37e1b70d9572cd9ab4ff1e
Mark Tiapkov <mark.tiapkov@gmail.com>
2020-08-13 10:35:40 -0700

    Initial HTML layout, initial JS code
    Co-authored-by: Vanessa Stewart <vstewart1213@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,20 @@ $(document).ready(function(){
   $("#formOne").submit(function(event){
     const seasonInput = parseInt($("#season").val());
 
+    // If they select 1 (Winter), then match them with Father Winter.
+    // If they select 2 (Spring), then match them with Lady of the Lake.
+    // If they select 3 (Summer), then match them with the Sun God.
+    // If they select 4 (Fall), then match them with Lady Gaga.
+
+    if (seasonInput === 1) {
+      $(".answers").text("You should date Father Winter")
+    } else if (seasonInput ===)
+
 
 
     $(".season").text(seasonInput);
 
+    $("asnwers"). 
 
 
 
