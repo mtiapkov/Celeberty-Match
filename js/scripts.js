96982ee57bd9d1de554d3b59d90ceae9a51ca723
Mark Tiapkov <mark.tiapkov@gmail.com>
2020-08-13 11:18:18 -0700

    Add functioning JS
    Co-authored-by: Vanessa Stewart <vstewart1213@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -3,28 +3,12 @@ $(document).ready(function(){
     event.preventDefault();
     const seasonInput = parseInt($("#season").val());
     const hogwartsInput = parseInt($("#hogwarts").val());
-    // If they select 1 (Winter) AND
-    // 1 (Gryf) , then "Date: Father Winter"
-    // 1 (Slyth) , then "Date: Father Christmas"
-
-
-
-    // If they select 2 (Spring), then match them with Lady of the Lake.
-    // If they select 3 (Summer), then match them with the Sun God.
-    // If they select 4 (Fall), then match them with Lady Gaga.
-    // If they select 1 (Gryffindor), 
-
-
-
-
 
     if (seasonInput === 1 && hogwartsInput === 1) {
       $(".answers").text("You should date Father Winter")
-
-      
-    } else if (seasonInput === 2){
+    } else if (seasonInput === 1 && hogwartsInput === 2){
       $(".answers").text("You should date Lady of the Lake")
-    } else if (seasonInput === 3){
+    } else if (seasonInput === 2 && hogwartsInput === 1){
       $(".answers").text("You should date the Sun God")
     } else {
       $(".answers").text("You should date Lady Gaga")
